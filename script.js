b67fd07bdf360ca55cb86625c6fd004295c42157
nastiadgaf <nastiadgaf@gmail.com>
2022-01-29 17:53:33 +0200

    Add animation

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 const box = $('#box');
     box.on('click', function(){
-        getRandomBgColor();
-        getRandomPosition();
-        getRandomSize();
+        let maxWidth = $(window).width();
+        let maxHeight = $(window).height();
+        let height = `${getRandomArbitrary(1, maxHeight - 100)}px`;
+        let width = `${getRandomArbitrary(1, maxWidth - 100)}px`;
+        box.animate({
+            top: height,
+            left: width,
+            width:`${getRandomArbitrary(1, 300)}px`,
+            height: `${getRandomArbitrary(1, 300)}px`,
+            backgroundColor: `#${getRandomColor()}`,
+        }, 1000)
+
+        console.log(getRandomColor())
     })
 
     function getRandomArbitrary(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
       }
 
-    function getRandomBgColor() {
-        let random = `#${Math.floor(Math.random()*16777215).toString(16)}`;
-        box.css("background-color", random );
-    }
-
-    function getRandomPosition() {
-        const maxWidth = $(window).width();
-        const maxHeight = $(window).height();
-        box.css("top", `${getRandomArbitrary(1, maxHeight)}px` );
-        box.css("left", `${getRandomArbitrary(1, maxWidth)}px` );
-    }
-
-    function getRandomSize() {
-        box.css("width", `${getRandomArbitrary(1, 300)}px` );
-        box.css("height", `${getRandomArbitrary(1, 300)}px` );
-    }
+    function getRandomColor(){
+          return Math.floor(Math.random()*16777215).toString(16);
+      }
